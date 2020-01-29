d1e4843d973cafbf4cb84c660828a994ccf252b8
letord <baptiste.letord@gmail.com>
2020-01-29 16:51:52 +0100

    fin exercice 2

--- app/src/main/java/com/appli/tp2/MainActivity.java
+++ app/src/main/java/com/appli/tp2/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -15,7 +16,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button envoyer=null;
+    Button envoyer = null;
     Button reset = null;
     EditText taille = null;
     EditText poids = null;
@@ -28,35 +29,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        envoyer = (Button)findViewById(R.id.calcul);
-        reset = (Button)findViewById(R.id.reset);
-        taille = (EditText)findViewById(R.id.taille);
-        poids = (EditText)findViewById(R.id.poids);
-        commentaire = (CheckBox)findViewById(R.id.commentaire);
-        group = (RadioGroup)findViewById(R.id.group);
-        result = (TextView)findViewById(R.id.result);
+        envoyer = (Button) findViewById(R.id.calcul);
+        reset = (Button) findViewById(R.id.reset);
+        taille = (EditText) findViewById(R.id.taille);
+        poids = (EditText) findViewById(R.id.poids);
+        commentaire = (CheckBox) findViewById(R.id.commentaire);
+        group = (RadioGroup) findViewById(R.id.group);
+        result = (TextView) findViewById(R.id.result);
 
 
         envoyer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String t= taille.getText().toString();
-                String p=poids.getText().toString();
+                String t = taille.getText().toString();
+                String p = poids.getText().toString();
                 float tValue = Float.valueOf(t);
                 float pValue = Float.valueOf(p);
 
-                if(tValue<=0){
-                    Toast.makeText(MainActivity.this, "la taille doit être positive",Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    if(pValue<=0){
-                        Toast.makeText(MainActivity.this, "le poids doit être positive",Toast.LENGTH_SHORT).show();
-                    }
-                    else{
-                        if (group.getCheckedRadioButtonId() == R.id.radio_centimetre) tValue = tValue / 100;
+                if (tValue <= 0) {
+                    Toast.makeText(MainActivity.this, "la taille doit être positive", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (pValue <= 0) {
+                        Toast.makeText(MainActivity.this, "le poids doit être positive", Toast.LENGTH_SHORT).show();
+                    } else {
+                        if (group.getCheckedRadioButtonId() == R.id.radio_centimetre)
+                            tValue = tValue / 100;
                         float imc = pValue / (tValue * tValue);
-                        String resultat="Votre IMC est " + imc+" . ";
-                        if(commentaire.isChecked()) resultat +=interpreteIMC(imc);
+                        String resultat = "Votre IMC est " + imc + " . ";
+                        if (commentaire.isChecked()) resultat += interpreteIMC(imc);
                         result.setText(resultat);
 
                     }
@@ -76,67 +76,46 @@ public class MainActivity extends AppCompatActivity {
         commentaire.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(((CheckBox)v).isChecked()) {
+                if (((CheckBox) v).isChecked()) {
                     result.setText(texteInit);
                 }
             }
         });
-        taille.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                result.setText(texteInit);
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-        poids.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                result.setText(texteInit);
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
+        taille.setOnKeyListener(modifListener);
+        poids.setOnKeyListener(modifListener);
 
 
     }
-    String interpreteIMC(double imc){
-        if(imc<16.5){
+
+    String interpreteIMC(double imc) {
+        if (imc < 16.5) {
             return "famine";
-        }
-        else if (16.5<imc && imc<18.5){
+        } else if (16.5 < imc && imc < 18.5) {
             return "maigreur";
-        }
-        else if (18.5<imc && imc<25){
+        } else if (18.5 < imc && imc < 25) {
             return "corpulence normale";
-        }
-        else if (25<imc && imc<30){
+        } else if (25 < imc && imc < 30) {
             return "surpoids";
-        }
-        else if (30<imc && imc<35){
+        } else if (30 < imc && imc < 35) {
             return "obésité modérée";
-        }
-        else if (35<imc && imc<40){
+        } else if (35 < imc && imc < 40) {
             return "obésité sévère";
-        }
-        else {
+        } else {
             return "obésité morbide ou massive";
         }
     }
+
+    // Se lance à chaque fois qu'on appuie sur une touche en étant sur un EditText
+    private View.OnKeyListener modifListener = new View.OnKeyListener() {
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            // On remet le texte à sa valeur par défaut
+            result.setText(texteInit);
+            String s = taille.getText().toString();
+            if (s.contains(".")) {
+                group.check(R.id.radio_metre);
+            }
+            return false;
+        }
+    };
 }
